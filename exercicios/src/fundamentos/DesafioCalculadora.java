9778615efbc312e57773fa4be6e4baa36e97ecb9
emiliosarges <emiliosarges@gmail.com>
2025-08-19 01:11:17 -0300

    desafio cumprido

---
+++ exercicios/src/fundamentos/DesafioCalculadora.java
@@ -0,0 +1,39 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+//DESAFIO: Fazer uma calculadora sem uasr IF ELSE. Usar somente o conteúdo das aultas até aqui. 
+
+public class DesafioCalculadora {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Digite o primeiro número: ");
+		Double num1 = entrada.nextDouble();
+		System.out.println("Digite o segundo número: ");
+		Double num2 = entrada.nextDouble();
+		entrada.nextLine();
+		System.out.println("Digite a operação que deseja fazer: +, -, *, /, ou % ");
+		String operacao = entrada.nextLine();
+		entrada.close();
+		
+		Double soma = num1 + num2;
+		Double subtracao = num1 - num2;
+		Double multiplicacao = num1 * num2;
+		Double divisao = num1 / num2;
+		Double modulo = num1 % num2;
+		
+		
+		
+		String resultado =	operacao.equals("+") ? soma.toString() :
+							operacao.equals("-") ? subtracao.toString() : 
+							operacao.equals("*") ? multiplicacao.toString() : 
+							operacao.equals("/") ? divisao.toString() : 
+							operacao.equals("%") ? modulo.toString() : "Operação Inválida";
+		
+		System.out.println(resultado);
+		
+		
+	}
+
+}
