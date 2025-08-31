f6f17d35602ef18a09e21c2f6945659bce1bb1c6
emiliosarges <emiliosarges@gmail.com>
2025-08-31 17:56:34 -0300

    Continuando os estudos

--- exercicios/src/colecoes/Pilha.java
+++ exercicios/src/colecoes/Pilha.java
@@ -1,5 +1,34 @@
 package colecoes;
 
-public class Pilha {
+import java.util.ArrayDeque;
+import java.util.Deque;
 
+public class Pilha {
+	public static void main(String[] args) {
+		Deque<String> livros = new ArrayDeque<>();
+		
+		livros.add("O pequeno Príncipe");
+		livros.push("Dom Quixote");
+		livros.push("O Hobbit");
+		
+		System.out.println(livros.peek());
+		System.out.println(livros.element());
+		
+		for(String livro: livros) {
+			System.out.println(livro);
+		}
+		
+		
+		System.out.println(livros.pop()); // gera exceção caso esteja vazia. 
+		System.out.println(livros.poll());
+		System.out.println(livros.poll());
+		System.out.println(livros.poll());
+		System.out.println(livros.poll());
+		//System.out.println(livros.remove());
+		
+		//livros.size();
+		//livros.clear();
+		//livros.contains("O pequeno principe");
+		//livros.isEmpty();
+	}
 }
